HewlettPackard/FAMfs

commit 9f9dd2ec0e9f7fcba7ca9e29ee8f4e664f6768f9

Add support for verbs provider to FAMfs

server/src/lf_client.c
@@ -16,6 +16,7 @@
 #include "famfs_env.h"
 #include "log.h"
 #include "lf_client.h"
+#include "unifycr_metadata.h"
 
 
 /* Parse LFS_COMMAND and start FAM emulation servers on nodes in hostlist */
@@ -28,8 +29,9 @@ int lfs_emulate_fams(char * const cmdline, int rank, int size,
     pthread_mutexattr_t pattr;
     pthread_condattr_t cattr;
     pid_t cpid;
+    size_t shm_size, len;
     char *argv[LFS_MAXARGS];
-    int argc, verbose;
+    int argc, verbose, srv_cnt, is_srv, zero_srv_rank, cnt;
     int rc = -1; /* OOM error */
 
     lfs_ctx_p = (LFS_CTX_t *) calloc(1, sizeof(LFS_CTX_t));
@@ -55,12 +57,15 @@ int lfs_emulate_fams(char * const cmdline, int rank, int size,
      */
 
     /* Initialize shared data */
-    lfs_shm = (LFS_SHM_t *) mmap(NULL, sizeof(LFS_SHM_t),
+    srv_cnt = params->node_servers;
+    shm_size = sizeof(LFS_SHM_t) + srv_cnt*sizeof(LFS_EXCG_t);
+    lfs_shm = (LFS_SHM_t *) mmap(NULL, shm_size,
             PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
     if (lfs_shm == NULL) {
         rc = -1;
         goto _exit;
     }
+    lfs_shm->node_servers = srv_cnt;
     lfs_shm->quit_lfs = 0;
     lfs_shm->lfs_ready = 0;
     pthread_mutexattr_init(&pattr);
@@ -80,17 +85,28 @@ int lfs_emulate_fams(char * const cmdline, int rank, int size,
         goto _exit;
     } else if (cpid == 0) {
         LF_SRV_t   **lf_servers;
-        int i, srv_cnt;
+        int i, part;
 
         /* On each node in nodelist */
-	i = find_my_node(params->nodelist, params->node_cnt, NULL);
+        i = find_my_node(params->nodelist, params->node_cnt, NULL);
         if (i >= 0) {
 
             /* Initialize libfabric target on node 'i' */
             rc = lf_servers_init(&lf_servers, params, i, 0);
-            if (rc)
+            if (rc) {
                 err("Can't start FAM emulation target on %s rc:%d",
                     params->nodelist[params->node_id], rc);
+            } else if (!params->lf_mr_flags.scalable) {
+                /* For each partition */
+                for (part = 0; part < srv_cnt; part++) {
+                    int lf_client_idx = to_lf_client_id(i, srv_cnt, part);
+
+                    if (params->lf_mr_flags.prov_key)
+                        lfs_shm->rmk[part].prov_key = params->mr_prov_keys[lf_client_idx];
+                    if (params->lf_mr_flags.virt_addr)
+                        lfs_shm->rmk[part].virt_addr = params->mr_virt_addrs[lf_client_idx];
+                }
+            }
         }
 
         /* It's Ok for parent process to proceed with MPI communication */
@@ -107,10 +123,9 @@ int lfs_emulate_fams(char * const cmdline, int rank, int size,
             pthread_mutex_unlock(&lfs_shm->lock);
         }
         pthread_cond_destroy(&lfs_shm->cond_quit);
-        munmap(lfs_shm, sizeof(LFS_SHM_t));
+        munmap(lfs_shm, shm_size);
 
         /* Close fabric and exit */
-        srv_cnt = params->node_servers;
         for (i = 0; i < srv_cnt; i++)
             lf_srv_free(lf_servers[i]);
         free(lf_servers);
@@ -138,6 +153,51 @@ int lfs_emulate_fams(char * const cmdline, int rank, int size,
 
     pthread_cond_destroy(&lfs_shm->cond_ready);
     lfs_ctx_p->child_pid = cpid;
+    if (rc || !(params->lf_mr_flags.prov_key || params->lf_mr_flags.virt_addr))
+        goto _exit;
+
+    /*
+     * LF remote key/address exchange
+     */
+
+    /* Create MPI communicator for LF servers */
+    is_srv = (params->node_id < 0)? 0 : 1;
+    zero_srv_rank = mpi_split_world(&params->mpi_comm, is_srv, 1, rank, size);
+    if (!is_srv || zero_srv_rank < 0 ||
+        !(params->lf_mr_flags.prov_key || params->lf_mr_flags.virt_addr))
+        goto _exit;
+
+    /* Exchange the keys within servers */
+    len = srv_cnt * sizeof(uint64_t);
+    if (params->lf_mr_flags.prov_key &&
+        ((rc = MPI_Allgather(MPI_IN_PLACE, len, MPI_BYTE,
+                             params->mr_prov_keys, len, MPI_BYTE,
+                             params->mpi_comm)))) {
+        LOG(LOG_ERR, "LF PROV_KEYS MPI_Allgather failed:%d", rc);
+        goto _close_comm;
+    }
+    if (params->lf_mr_flags.virt_addr &&
+        ((rc = MPI_Allgather(MPI_IN_PLACE, len, MPI_BYTE,
+                             params->mr_virt_addrs, len, MPI_BYTE,
+                             params->mpi_comm)))) {
+        LOG(LOG_ERR, "LF VIRT_ADDRS MPI_Allgather failed:%d", rc);
+        //goto _close_comm;
+    }
+_close_comm:
+    MPI_Comm_free(&params->mpi_comm);
+
+    /* Broadcast the keys to all [clients] */
+    cnt = params->fam_cnt * srv_cnt;
+    if (params->lf_mr_flags.prov_key &&
+        ((rc = mpi_broadcast_arr64(params->mr_prov_keys, cnt, zero_srv_rank)))) {
+        LOG(LOG_ERR, "LF PROV_KEYS MPI broadcast failed:%d", rc);
+        goto _exit;
+    }
+    if (params->lf_mr_flags.virt_addr &&
+        ((rc = mpi_broadcast_arr64(params->mr_virt_addrs, cnt, zero_srv_rank)))) {
+        LOG(LOG_ERR, "LF VIRT_ADDRS MPI broadcast failed:%d", rc);
+        //goto _exit;
+    }
 
 _exit:
     if (rc) {
@@ -160,11 +220,39 @@ void free_lfs_ctx(LFS_CTX_t **lfs_ctx_pp) {
             pthread_mutex_unlock(&lfs_shm->lock);
             pthread_cond_signal(&lfs_shm->cond_quit);
         }
-        munmap(lfs_ctx_p->lfs_shm, sizeof(LFS_SHM_t));
+        munmap(lfs_ctx_p->lfs_shm,
+               sizeof(LFS_SHM_t) + lfs_shm->node_servers*sizeof(LFS_EXCG_t));
     }
 
     free_lf_params(&params);
     free(lfs_ctx_p);
     *lfs_ctx_pp = NULL;
+}
+
+int meta_register_fam(LFS_CTX_t *lfs_ctx)
+{
+    N_PARAMS_t *params = lfs_ctx->lf_params;
+    fam_attr_val_t *fam_attr;
+    LFS_EXCG_t *rmk, *attr;
+    unsigned int fam_id, part_cnt, i;
+    int rc;
+
+    /* Do nothing on LF client */
+    if (params->node_id < 0)
+        return 0;
+
+    /* NOTE: FAM emulation only; limited to 31 bits */
+    fam_id = (unsigned int) params->node_id;
+    part_cnt = params->node_servers;
+    fam_attr = (fam_attr_val_t *)malloc(fam_attr_val_sz(part_cnt));
+    fam_attr->part_cnt = part_cnt;
+    attr = fam_attr->part_attr;
+    rmk = lfs_ctx->lfs_shm->rmk;
+    for (i = 0; i < part_cnt; i++, rmk++, attr++)
+        memcpy(attr, rmk, sizeof(LFS_EXCG_t));
+
+    rc = meta_famattr_put(fam_id, fam_attr);
+    free(fam_attr);
+    return rc;
 }
 

client/src/lf_client.c
@@ -11,10 +11,44 @@
 #include <limits.h>
 
 #include "famfs_env.h"
+#include "famfs_global.h"
 #include "lf_client.h"
 #include "fam_stripe.h"
-//#include "unifycr-internal.h"
+#include "unifycr-internal.h" /* DEBUG() macro */
 
+
+static int get_lf_meta(N_PARAMS_t *params)
+{
+    fam_attr_val_t *fam_meta = NULL;
+    int fam_cnt, srv_cnt;
+    int i, part, lf_client_idx;
+    int rc = 0;
+
+    if (params->lf_mr_flags.scalable ||
+        (params->mr_prov_keys == NULL && params->mr_virt_addrs == NULL))
+        return UNIFYCR_SUCCESS;
+
+    fam_cnt = params->fam_cnt;
+    srv_cnt = params->node_servers;
+    for (i = 0; i < fam_cnt; i++) {
+        rc = get_global_fam_meta(i, &fam_meta);
+        if (rc != UNIFYCR_SUCCESS) {
+            err("Get LF meta error:%d id:%d", rc, i);
+            return -1;
+        }
+        for (part = 0; part < srv_cnt && fam_meta; part++) {
+            lf_client_idx = to_lf_client_id(i, srv_cnt, part);
+            params->mr_prov_keys[lf_client_idx] = fam_meta->part_attr[part].prov_key;
+            params->mr_virt_addrs[lf_client_idx] = fam_meta->part_attr[part].virt_addr;
+            DEBUG("get_lf_meta id:%d(p%d) prov_key:%lu virt_addr:%lu\n",
+              i, part,
+              params->mr_prov_keys[lf_client_idx],
+              params->mr_virt_addrs[lf_client_idx]);
+        }
+        free(fam_meta);
+    }
+    return rc;
+}
 
 static int alloc_lf_clients(int argc, char **argv, int rank, N_PARAMS_t **params_p)
 {
@@ -30,6 +64,12 @@ static int alloc_lf_clients(int argc, char **argv, int rank, N_PARAMS_t **params
     params = *params_p;
     ASSERT(params);
     params->w_thread_cnt = 1;
+
+    if ((rc = get_lf_meta(params))) {
+	DEBUG("rank:%d failed to get LF metadata from the delegator on mount, error:%d",
+	      dbg_rank, rc);
+	return rc;
+    }
 
     rc = lf_clients_init(params);
     if (rc == 0)

client/src/unifycr-fixed.c
@@ -487,6 +487,8 @@ int lf_write(char *buf, size_t len,  int chunk_phy_id, off_t chunk_offset, int *
     //size_t transfer_sz;
     off_t off;
     //int i, blocks;
+    ssize_t wcnt;
+    uint64_t *event, evnt;
     int dst_node, rc = 0;
     ALLOCA_CHUNK_PR_BUF(pr_buf);
 
@@ -506,9 +508,12 @@ int lf_write(char *buf, size_t len,  int chunk_phy_id, off_t chunk_offset, int *
 
     if (lfs_params->use_cq) {
         tx_cq = node->tx_cqq[0];
+        evnt = 0;
+        event = &evnt;
     } else {
         cntr = node->wcnts[0];
         chunk->w_event = fi_cntr_read(cntr);
+        event = &chunk->w_event;
     }
 
     //transfer_sz = params->transfer_sz;
@@ -521,12 +526,13 @@ int lf_write(char *buf, size_t len,  int chunk_phy_id, off_t chunk_offset, int *
     off = chunk_offset + 1ULL * fam_stripe->stripe_in_part * lfs_params->chunk_sz;
     if (trg_off)
         *trg_off = off;
+    off += node->dst_virt_addr;
 
     stats_fi_wr = lfs_ctx->famsim_stats_fi_wr;
     STATS_START(start);
 
     //for (i = 0; i < blocks; i++) {
-    DEBUG("%d: write chunk:%d @%jd to %u/%u/%s(@%lu) on FAM module %d(p%d) len:%zu desc:%p off:%jd mr_key:%lu",
+    DEBUG("%d: write chunk:%d @%jd to %u/%u/%s(@%lu) on FAM module %d(p%d) len:%zu desc:%p off:%016lx mr_key:%lu",
         lfs_params->node_id, chunk_phy_id, chunk_offset,
         fam_stripe->extent, fam_stripe->stripe_in_part, pr_chunk(pr_buf, chunk->data, chunk->parity), (unsigned long)*tgt_srv_addr,
         dst_node, node->partition,
@@ -534,12 +540,33 @@ int lf_write(char *buf, size_t len,  int chunk_phy_id, off_t chunk_offset, int *
 
     famsim_stats_start(famsim_ctx, stats_fi_wr);
 
-    ON_FI_ERROR(
-        fi_write(tx_ep, buf, len, lookup_mreg(buf, len, chunk->lf_client_idx), 
-            *tgt_srv_addr, off, node->mr_key, (void*)buf),
-        "%d (%s): fi_write failed on FAM module %d(p%d)", 
-        lfs_params->node_id, nodelist[lfs_params->node_id],
-        dst_node, fam_stripe->partition);
+    do {
+        rc = fi_write(tx_ep, buf, len, lookup_mreg(buf, len, chunk->lf_client_idx),
+                      *tgt_srv_addr, off, node->mr_key, (void*)buf);
+        if (rc == 0) {
+            (*event)++;
+            break;
+        } else if (rc < 0 && rc != -FI_EAGAIN)
+            break;
+
+        if (lfs_params->use_cq) {
+            int ret;
+
+            wcnt = 0;
+            ret = lf_check_progress(tx_cq, &wcnt);
+            if (ret < 0) {
+                rc = ret;
+                err("lf_check_progress error:%d - %m", rc);
+                break;
+            }
+            if (wcnt)
+                (*event)--;
+        }
+    } while (rc == -FI_EAGAIN);
+    /* TODO: Return I/O error */
+    ON_FI_ERROR(rc, "%d (%s): fi_write failed on FAM module %d(p%d)",
+                    lfs_params->node_id, nodelist[lfs_params->node_id],
+                    dst_node, fam_stripe->partition);
 
     if (fam_stripe->extent == 0 && fam_stripe->stripe_in_part == 0)
         famsim_stats_stop(stats_fi_wr, 1);
@@ -551,10 +578,9 @@ int lf_write(char *buf, size_t len,  int chunk_phy_id, off_t chunk_offset, int *
     //}
 
     if (!lfs_params->use_cq) {
-        chunk->w_event++;
         rc = fi_cntr_wait(cntr, chunk->w_event, lfs_params->io_timeout_ms);
         if (rc == -FI_ETIMEDOUT) {
-            err("%d (%s): lf_write timeout chunk:%d to %u/%u/%s on FAM module %d(p%d) len:%zu off:%jd",
+            err("%d (%s): lf_write timeout chunk:%d to %u/%u/%s on FAM module %d(p%d) len:%zu off:%016lx",
                 lfs_params->node_id, nodelist[lfs_params->node_id], chunk_phy_id,
                 fam_stripe->extent, fam_stripe->stripe_in_part, pr_chunk(pr_buf, chunk->data, chunk->parity),
                 dst_node, node->partition, len, off);
@@ -568,11 +594,11 @@ int lf_write(char *buf, size_t len,  int chunk_phy_id, off_t chunk_offset, int *
             ON_FI_ERROR(fi_cntr_seterr(cntr, 0), "failed to reset counter error!");
         }
     } else {
-        ssize_t wcnt = 0;
-        do {        
-            if ((rc = lf_check_progress(tx_cq, &wcnt)) < 0)
-                break;
-        } while (wcnt < 1);
+        wcnt = *event;
+        do {
+            rc = lf_check_progress(tx_cq, &wcnt);
+        } while (rc == 0 && wcnt > 0);
+        // if (rc == 0) *event = 0;
     }
 
     UPDATE_STATS(lf_wr_stat, 1, len, start);
@@ -593,18 +619,19 @@ int lf_fam_read(char *buf, size_t len, off_t fam_off, int nid, unsigned long cid
     struct fid_cq *tx_cq = NULL;
     //size_t transfer_sz;
     //int i, blocks;
+    ssize_t wcnt;
+    uint64_t *event, evnt;
     int src_node, rc = 0;
     ALLOCA_CHUNK_PR_BUF(pr_buf);
 
     /* to FAM chunk */
-    
     chunk = get_fam_chunk(cid, fam_stripe, &src_node);
     if (chunk == NULL) {
 	DEBUG("%d chunk:%jd - ENOSPC\n", lfs_params->node_id, cid);
 	errno = ENOSPC;
 	return UNIFYCR_ERR_NOSPC;
     }
-   
+
     node = lfs_params->lf_clients[nid];
     ASSERT(src_node == node->node_id);
     ASSERT(src_node == nid);
@@ -615,9 +642,12 @@ int lf_fam_read(char *buf, size_t len, off_t fam_off, int nid, unsigned long cid
 
     if (lfs_params->use_cq) {
         tx_cq = node->tx_cqq[0];
+        evnt = 0;
+        event = &evnt;
     } else {
         cntr = node->rcnts[0];
         chunk->r_event = fi_cntr_read(cntr);
+        event = &chunk->w_event;
     }
     //transfer_sz = params->transfer_sz;
     //int blocks = len / transfer_sz;
@@ -628,20 +658,42 @@ int lf_fam_read(char *buf, size_t len, off_t fam_off, int nid, unsigned long cid
 
     STATS_START(start);
 
-    off_t coff = fam_off - 1ULL*fam_stripe->stripe_in_part*lfs_params->chunk_sz; 
-    DEBUG("%d: read chunk:%jd @%lu to %u/%u/%s(@%lu) on FAM module %d(p%d) len:%zu desc:%p off:%jd mr_key:%lu",
+    off_t coff = fam_off - 1ULL*fam_stripe->stripe_in_part*lfs_params->chunk_sz;
+    fam_off += node->dst_virt_addr;
+    DEBUG("%d: read chunk:%jd @%lu to %u/%u/%s(@%lu) on FAM module %d(p%d) len:%zu desc:%p off:%016lx mr_key:%lu",
 	  lfs_params->node_id, cid, coff,
 	  fam_stripe->extent, fam_stripe->stripe_in_part, pr_chunk(pr_buf, chunk->data, chunk->parity), (unsigned long)*tgt_srv_addr,
 	  src_node, node->partition,
 	  len, node->local_desc[0], fam_off, node->mr_key);
 
-    ON_FI_ERROR(
-        fi_read(tx_ep, buf, len, lookup_mreg(buf, len, nid), 
-            *tgt_srv_addr, fam_off, node->mr_key, (void*)buf),
-        "%d: fi_read failed on FAM module %d(p%d)", lfs_params->node_id, src_node, fam_stripe->partition);
+    do {
+        rc = fi_read(tx_ep, buf, len, lookup_mreg(buf, len, nid),
+                     *tgt_srv_addr, fam_off, node->mr_key, (void*)buf);
+        if (rc == 0) {
+            (*event)++;
+            break;
+        } else if (rc < 0 && rc != -FI_EAGAIN)
+            break;
+
+        if (lfs_params->use_cq) {
+            int ret;
+
+            wcnt = 0;
+            ret = lf_check_progress(tx_cq, &wcnt);
+            if (ret < 0) {
+                rc = ret;
+                err("lf_check_progress error:%d - %m", rc);
+                break;
+            }
+            if (wcnt)
+                (*event)--;
+        }
+    } while (rc == -FI_EAGAIN);
+    /* TODO: Return I/O error */
+    ON_FI_ERROR(rc, "%d: fi_read failed on FAM module %d(p%d)",
+                    lfs_params->node_id, src_node, fam_stripe->partition);
 
     if (!lfs_params->use_cq) {
-        chunk->r_event++;
         rc = fi_cntr_wait(cntr, chunk->r_event, lfs_params->io_timeout_ms);
         if (rc == -FI_ETIMEDOUT) {
             err("%d: lf_read timeout chunk:%jd to %u/%u/%s on FAM module %d(p%d) len:%zu off:%jd",
@@ -658,10 +710,11 @@ int lf_fam_read(char *buf, size_t len, off_t fam_off, int nid, unsigned long cid
             ON_FI_ERROR(fi_cntr_seterr(cntr, 0), "failed to reset counter error!");
         }
     } else {
-        ssize_t rcnt = 0;
-        do {        
-            rc = lf_check_progress(tx_cq, &rcnt);
-        } while (rc >= 0 && rcnt < 1);
+        wcnt = *event;
+        do {
+            rc = lf_check_progress(tx_cq, &wcnt);
+        } while (rc == 0 && wcnt > 0);
+        // if (rc == 0) *event = 0;
     }
 
     UPDATE_STATS(lf_rd_stat, 1, len, start);
@@ -788,7 +841,7 @@ static int unifycr_logio_chunk_write(
             //chunk_offset = off_in_chunk(pos);
 
             chunk_phy_id = physical_chunk_id(meta, chunk_id);
-            DEBUG("%d: write %zu bytes @%d phy_chunk:%d @%lu\n",
+            DEBUG("%d: write %zu bytes chunk:%d phy_chunk:%d @%lu\n",
                   lfs_ctx->lfs_params->node_id, count, chunk_id, chunk_phy_id, chunk_offset);
             int rc = lf_write((char *)buf, count, chunk_phy_id, chunk_offset, &my_node, &my_off);
             if (rc) {

client/src/unifycr-internal.h
@@ -587,4 +587,7 @@ int unifycr_match_received_ack(read_req_t *read_req, int count,
                                read_req_t *match_req);
 int unifycr_locate_req(read_req_t *read_req, int count,
                        read_req_t *match_req);
+int lf_connect(char *addr, char *srvc);
+int get_global_fam_meta(int fam_id, fam_attr_val_t **fam_meta);
+
 #endif /* UNIFYCR_INTERNAL_H */

client/src/unifycr-sysio.c
@@ -1551,7 +1551,7 @@ static ssize_t match_rq_and_read(read_req_t *rq, int rq_cnt, fsmd_kv_t  *md, int
             }
 
             if (fam_len) {
-                DEBUG("rq read %lu[%lu]@%lu, nid=%jd, cid=%jd \n", fam_len, bufp - rq[i].buf, fam_off, 
+                DEBUG("rq read %lu[%lu]@%lu, nid=%jd, cid=%jd \n", fam_len, bufp - rq[i].buf, fam_off,
                     md[j].v.node, md[j].v.chunk);
                 if ((rc = lf_fam_read(bufp, fam_len, fam_off, md[j].v.node, md[j].v.chunk))) {
                     ioerr("lf_fam_read failed ret:%d", rc);

client/src/unifycr.c
@@ -103,7 +103,6 @@ typedef int64_t off64_t;
 #include "famfs_env.h"
 #include "famfs_stats.h"
 #include "lf_client.h"
-#include "famfs_global.h"
 
 
 static int unifycr_fpos_enabled   = 1;  /* whether we can use fgetpos/fsetpos */
@@ -1156,6 +1155,74 @@ static int get_global_file_meta(int gfid, unifycr_fattr_t **file_meta)
     memcpy(*file_meta, cmd_buf + 2 * sizeof(int), sizeof(unifycr_fattr_t));
     return UNIFYCR_SUCCESS;
 }
+
+int get_global_fam_meta(int fam_id, fam_attr_val_t **fam_meta)
+{
+    fam_attr_val_t hdr;
+    size_t size;
+    int cmd = COMM_META;
+    int flag = 4;
+
+    memcpy(cmd_buf, &cmd, sizeof(int));
+    memcpy(cmd_buf + sizeof(int), &flag, sizeof(int));
+    memcpy(cmd_buf + sizeof(int) + sizeof(int),
+           &fam_id, sizeof(int));
+
+    int rc = __real_write(client_sockfd, cmd_buf, sizeof(cmd_buf));
+    if (rc != 0) {
+        int bytes_read = 0;
+        cmd_fd.events = POLLIN | POLLPRI;
+        cmd_fd.revents = 0;
+
+        rc = poll(&cmd_fd, 1, -1);
+
+        if (rc == 0) {
+            /* encounter timeout*/
+            return -1;
+        } else {
+            if (rc > 0) {
+                if (cmd_fd.revents != 0) {
+                    if (cmd_fd.revents == POLLIN) {
+                        bytes_read = __real_read(client_sockfd,
+                                                 cmd_buf, sizeof(cmd_buf));
+                        if (bytes_read == 0) {
+                            /*remote connection is closed*/
+                            return -1;
+                        } else {
+                            if (*((int *)cmd_buf) != COMM_META || *((int *)cmd_buf + 1)
+                                != ACK_SUCCESS) {
+                                *fam_meta = NULL;
+                                return -1;
+                            } else {
+                                /*success*/
+
+                            }
+                        }
+                    } else {
+                        /*encounter connection error*/
+                        return -1;
+                    }
+                } else {
+                    /*file descriptor is negative*/
+                    return -1;
+                }
+            } else {
+                /* encounter error*/
+                return -1;
+            }
+        }
+    } else {
+        /*write error*/
+        return -1;
+    }
+
+    memcpy(&hdr, cmd_buf + 2 * sizeof(int), sizeof(fam_attr_val_t));
+    size = fam_attr_val_sz(hdr.part_cnt);
+    *fam_meta = (fam_attr_val_t *)malloc(size);
+    memcpy(*fam_meta, cmd_buf + 2 * sizeof(int), size);
+    return UNIFYCR_SUCCESS;
+}
+
 /*
  * insert file attribute to attributed share memory buffer
  * */

client/src/unifycr.h
@@ -108,7 +108,6 @@ chunk_list_t *unifycr_get_chunk_list(char *path);
  * and to test above function*/
 void unifycr_print_chunk_list(char *path);
 
-int lf_connect(char *addr, char *srvc);
 int famfs_buf_reg(char *buf, size_t len, void **ctx);
 int famfs_buf_unreg(void *ctx);
 

common/src/famfs_global.h
@@ -1,7 +1,9 @@
 #ifndef FAMFS_GLOBAL_H_
-#define FAMFS_GLOBAL_H_ 
+#define FAMFS_GLOBAL_H_
 
 #include <sys/types.h>
+#include <inttypes.h>
+
 
 typedef enum {
     COMM_MOUNT     = 0x1, /*the list of addrs: appid, size of buffer, offset of data section, metadata section*/
@@ -45,7 +47,6 @@ typedef struct {
     };
 } fsmd_val_t;
 
-
 typedef struct {
     fsmd_key_t   k;
     fsmd_val_t   v;
@@ -62,6 +63,27 @@ typedef struct {
     size_t          cnt;
     lf_mreg_t       *regs;
 } famfs_mr_list_t;
+
+typedef struct lfs_excg_ {
+	uint64_t	prov_key;	/* memory registration key */
+	uint64_t	virt_addr;	/* remote virtual address */
+} LFS_EXCG_t;
+
+typedef struct {
+	uint32_t	_f __attribute__((aligned(8))); /* reserved */
+	uint32_t	part_cnt;	/* number of partitions on this FAM */
+	LFS_EXCG_t	part_attr[];	/* partition LF remote key & virt addr */
+} __attribute__((packed))  fam_attr_val_t;
+#define fam_attr_val_sz(cnt) (sizeof(fam_attr_val_t) + (cnt)*sizeof(LFS_EXCG_t))
+
+typedef struct {
+	uint64_t	fam_id;		/* FAM Id */
+	uint32_t	_f;		/* reserved */
+	uint32_t	part_cnt;	/* number of partitions on this FAM */
+	LFS_EXCG_t	part_attr[];	/* partition LF remote key & virt addr */
+} fam_attr_t;
+#define fam_attr_sz(cnt) (sizeof(fam_attr_t) + (cnt)*sizeof(LFS_EXCG_t))
+
 
 extern famfs_mr_list_t known_mrs;
 

commit 57c529222e2b09790036df3345f539966839e866

Fix FAM support broken by commit 9f9dd2ec0e

server/src/lf_client.c
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2018-2019, HPE
+ *
+ * Written by: Dmitry Ivanov
+ */
+
+#include <assert.h>
 #include <unistd.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -234,24 +241,44 @@ int meta_register_fam(LFS_CTX_t *lfs_ctx)
     N_PARAMS_t *params = lfs_ctx->lf_params;
     fam_attr_val_t *fam_attr;
     LFS_EXCG_t *rmk, *attr;
+    FAM_MAP_t *fam_map;
     unsigned int fam_id, part_cnt, i;
-    int rc;
+    int node_id, rc;
 
     /* Do nothing on LF client */
+    node_id = params->node_id;
     if (params->node_id < 0)
-        return 0;
+	return 0;
 
-    /* NOTE: FAM emulation only; limited to 31 bits */
-    fam_id = (unsigned int) params->node_id;
     part_cnt = params->node_servers;
     fam_attr = (fam_attr_val_t *)malloc(fam_attr_val_sz(part_cnt));
     fam_attr->part_cnt = part_cnt;
     attr = fam_attr->part_attr;
-    rmk = lfs_ctx->lfs_shm->rmk;
-    for (i = 0; i < part_cnt; i++, rmk++, attr++)
-        memcpy(attr, rmk, sizeof(LFS_EXCG_t));
 
-    rc = meta_famattr_put(fam_id, fam_attr);
+    /* Having the real FAM? */
+    fam_map = params->fam_map;
+    if (fam_map) {
+	unsigned long long *ids;
+
+	assert(node_id>=0);
+	assert(part_cnt==1); /* TODO: Support FAM partitioning */
+	ids = fam_map->fam_ids[node_id];
+	rc = 0;
+	for (i = 0; i < fam_map->node_fams[node_id]; i++) {
+	    fam_id = (unsigned int) ids[i];
+	    /* TODO: Read device pk&offset from configuration */
+	    attr->prov_key = 0;
+	    attr->virt_addr = 0;
+	    rc |= meta_famattr_put(fam_id, fam_attr);
+	}
+    } else {
+	/* NOTE: FAM emulation only; limited to 31 bits */
+	fam_id = (unsigned int) params->node_id;
+	rmk = lfs_ctx->lfs_shm->rmk;
+	for (i = 0; i < part_cnt; i++, rmk++, attr++)
+	    memcpy(attr, rmk, sizeof(LFS_EXCG_t));
+	rc = meta_famattr_put(fam_id, fam_attr);
+    }
     free(fam_attr);
     return rc;
 }