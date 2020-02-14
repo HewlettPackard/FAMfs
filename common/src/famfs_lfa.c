3a2541de8f9640e6d5d8730727d89b2a2ea58afe
Oleg Neverovitch <oleg.neverovitch@hpe.com>
2020-02-13 21:41:54 -0700

    release 1.0 of atomics

---
+++ common/src/famfs_lfa.c
@@ -0,0 +1,456 @@
+#include <sched.h>
+#include "famfs_lfa.h"
+
+#define ON_FIERR(action, onerr, msg, ...)   \
+    do {                                    \
+        int64_t __err;                      \
+        if ((__err = (action))) {           \
+            fprintf(stderr, "lfa error @%s:%d" #msg ": %ld - %s\n", __FUNCTION__, __LINE__, ## __VA_ARGS__, \
+                    __err, fi_strerror(abs(__err))); \
+            onerr;                          \
+        }                                   \
+    } while (0);
+
+#define LOG_FIERR(err, msg, ...)            \
+    do {                                    \
+        int64_t __err = (err);              \
+        fprintf(stderr, "lfa error @%s:%d" #msg ": %ld - %s\n", __FUNCTION__, __LINE__, ## __VA_ARGS__, \
+                    __err, fi_strerror(abs(err))); \
+    } while (0);
+
+
+static ssize_t _wait_cq(F_LFA_DESC_t *lfa) {
+
+    ssize_t ret = 0, rc;
+    struct fi_cq_err_entry err_entry;
+
+    bzero(&err_entry, sizeof(err_entry));
+
+    while (1) {
+        ret = fi_cq_read(lfa->cq, &lfa->cqe, 1);
+        if (ret >= 0) {
+            return 0;
+        }
+        if (ret == -FI_EAGAIN) {
+            sched_yield();
+            continue;
+        } else if (ret != -FI_EAVAIL) {
+            LOG_FIERR(ret, "fi_cq_read: error, more data available");
+        }
+        rc = fi_cq_readerr(lfa->cq, &err_entry, 0);
+        if (!rc)
+            /* Possibly no error? If so, retry. */
+            sched_yield(); 
+            continue;
+        if (rc > 0) {
+            ret = -err_entry.err;
+            LOG_FIERR(ret, "cq error");
+            return ret;
+        }
+        if (rc == -FI_EAGAIN) {
+            /* Possible no error? If so, retry. */
+            sched_yield(); 
+            continue;
+        }
+        LOG_FIERR(rc, "fi_cq_readerr");
+        return rc;
+    }
+
+    return 0;
+}
+
+
+/*
+   Create atomic blob, open new endpoint, translate all addresses etc
+     In
+        dom     domain object - see above
+        noav    if = 1, do not create AV
+        fi      fabric info obtained when domain was created
+     Out
+        plfa    address of a pointer to hold created LFA structure
+     Return
+        0       success
+      <>0       error, see errno/fi_errno
+*/
+int f_lfa_create(struct fid_domain *dom, struct fid_av *av, struct fi_info *fi, F_LFA_DESC_t **plfa) {
+    struct fi_cq_attr   cq_attr;
+    struct fi_av_attr   av_attr;
+    int rc = 0;
+
+    F_LFA_DESC_t *lfa = calloc(1, sizeof(F_LFA_DESC_t));
+    if (!lfa)
+        return ENOMEM;
+
+    pthread_mutex_init(&lfa->lock, NULL);
+    ON_FIERR(rc = fi_endpoint(dom, fi, &lfa->ep, NULL), goto _clean, "create ep");
+
+    memset(&cq_attr, 0, sizeof(cq_attr));
+    cq_attr.format = FI_CQ_FORMAT_CONTEXT;
+    cq_attr.wait_obj = FI_WAIT_NONE;
+    cq_attr.size = fi->tx_attr->size;
+    ON_FIERR(rc = fi_cq_open(dom, &cq_attr, &lfa->cq, NULL), goto _clean1, "open cq");
+    ON_FIERR(rc = fi_ep_bind(lfa->ep, &lfa->cq->fid, FI_RECV | FI_TRANSMIT), goto _clean2, "bind cq");
+
+    if (!av) {
+        memset(&av_attr, 0, sizeof(av_attr));
+        av_attr.type = FI_AV_TABLE;
+        ON_FIERR(rc = fi_av_open(lfa->dom, &av_attr, &lfa->av, NULL), goto _clean2, "av open");
+    } else {
+        lfa->av = av;
+    }
+    ON_FIERR(rc = fi_ep_bind(lfa->ep, &lfa->av->fid, 0), goto _clean2, "av bind");
+
+    ON_FIERR(rc = fi_enable(lfa->ep), goto _clean2, "ep enable");
+
+    lfa->dom = dom;
+    *plfa = lfa;
+    return 0;
+
+_clean2:
+
+    if (lfa->cq)
+        fi_close(&lfa->cq->fid);
+    if (lfa->av)
+        fi_close(&lfa->av->fid);
+
+_clean1:
+    if (lfa->ep)
+        fi_close(&lfa->ep->fid);
+
+_clean:
+    free(lfa);
+    *plfa = NULL;
+    return rc;
+}
+
+/*
+   Register local buffers for remote atomic blob access
+   To be called only by nodes that provide memory for atomic ops (servers)
+     In
+        lfa:        LFA descriptor
+        key:        protection key to be used for MR
+        bsize:      size of the memory buffer to be served out
+     Inout
+        pbuf:       address of a pointer to data buffer to be served out
+                    if *pbuf == NULL, memory will be allocated
+     Out
+        pabd:       (new) blob desriptor pointer
+     Return
+        0           success
+        EEXISTS     a blob with this key already registered
+        EINVAL      input buffer exists and its size != bsize
+        <>0         error
+*/
+int f_lfa_register(F_LFA_DESC_t *lfa, uint64_t key, size_t bsize, void **pbuf, F_LFA_ABD_t **pabd) {
+    F_LFA_ABD_t *abd;
+    void *mem;
+    int  rc = 0, f = 0;
+
+    if (posix_memalign((void **)&abd, 4096, sizeof(F_LFA_ABD_t))) 
+        return ENOMEM;
+    bzero(abd, sizeof(F_LFA_ABD_t));
+    *pabd = abd;
+
+    if (!*pbuf) {
+        mem = calloc(1, bsize);
+        if (!mem)
+            return ENOMEM;
+        *pbuf = mem;
+        f++;
+    } else {
+        mem = *pbuf;
+    }
+    abd->srv_bsz = bsize;
+    abd->srv_buf = mem;
+    abd->srv_key = key;
+
+    ON_FIERR(rc = fi_mr_reg(lfa->dom, mem, bsize, FI_REMOTE_READ|FI_REMOTE_WRITE, 
+                            0, key, 0, &abd->srv_mr, NULL), goto _clean, "mr reg");
+
+    if ((rc = pthread_mutex_lock(&lfa->lock)))
+        goto _clean;
+
+    abd->next = lfa->blobs;
+    lfa->blobs = abd;
+    abd->lfa = lfa;
+    pthread_mutex_unlock(&lfa->lock);
+
+    return 0;
+
+_clean:
+    free(abd);
+    *pabd = NULL;
+    if (f) {
+        free(mem);
+        *pbuf = NULL;
+    }
+    return rc;
+}
+
+/*
+   Attach to remote atomic blob
+   To be used by any and all nodes that performs atomic ops (clients). This list may or may
+   not include servers. I.e. servers could be subset of clients
+     In
+        lfa:        LFA descriptor
+        key:        remote memory protection key
+        nlist:      list of remote nodes names to perform atomic ops on (servers)
+        bsize:      size of the input memory buffer
+     Inout
+        ibuf:       address of a pointer to input buffer
+                    if *ibuf == NULL, memory will be allocated
+     Out
+        pbdb:       (new) blob desriptor pointer
+     Return
+        0           success
+        EEXISTS     a blob with this key already attached
+        EINVAL      remote buffer exists and its size != bsize
+        <>0         error
+*/
+int f_lfa_attach(F_LFA_DESC_t *lfa, uint64_t key, F_LFA_SLIST_t *lst, int lcnt, size_t bsize, void **ibuf, F_LFA_ABD_t **pabd) {
+    F_LFA_ABD_t *abd;
+    void *mem;
+    int  rc = 0, f = 0;
+
+    if (posix_memalign((void **)&abd, 4096, sizeof(F_LFA_ABD_t))) 
+        return ENOMEM;
+    bzero(abd, sizeof(F_LFA_ABD_t));
+    *pabd = abd;
+
+    if (!*ibuf) {
+        mem = calloc(1, bsize);
+        if (!mem)
+            return ENOMEM;
+        *ibuf = mem;
+        f++;
+    } else {
+        mem = *ibuf;
+    }
+    abd->in_bsz = bsize;
+    abd->in_buf = mem;
+    abd->srv_key = key;
+    abd->ops_key = key + F_LFA_LK_BASE;
+    ON_FIERR(rc = fi_mr_reg(lfa->dom, abd->ops, sizeof(abd->ops), FI_READ|FI_WRITE, 
+                            0, abd->ops_key, 0, &abd->ops_mr, NULL), goto _clean, "mr reg");
+    abd->tadr = calloc(lcnt, sizeof(fi_addr_t));
+    if (!abd->tadr) {
+        rc = ENOMEM;
+        goto _clean;
+    }
+    abd->ops_mr_dsc = fi_mr_desc(abd->ops_mr);
+    if (!abd->ops_mr_dsc) {
+        LOG_FIERR(-FI_EBADFLAGS, "bad MR descriptor");
+        return -FI_EBADFLAGS;
+    }
+
+    abd->nsrv = lcnt;
+    abd->slist = calloc(1, sizeof(F_LFA_SLIST_t)*lcnt);
+    for (int i = 0; i < lcnt; i++) {
+        rc = fi_av_insertsvc(lfa->av, lst[i].name, lst[i].service, &abd->tadr[i], 0, NULL);
+        ON_FIERR(rc == 1 ? 0 : rc, goto _clean, "av svc insert");
+        abd->slist[i].name = strdup(lst[i].name);
+        abd->slist[i].service = strdup(lst[i].service);
+    }
+
+    if ((rc = pthread_mutex_lock(&lfa->lock)))
+        goto _clean;
+
+    abd->next = lfa->blobs;
+    lfa->blobs = abd;
+    abd->lfa = lfa;
+
+    pthread_mutex_unlock(&lfa->lock);
+
+    return 0;
+
+_clean:
+    if (abd->ops_mr)
+        fi_close(&abd->ops_mr->fid);
+
+    if(abd->tadr)
+        free(abd->tadr);
+
+    free(abd);
+    *pabd = NULL;
+    if (f) {
+        free(mem);
+        *ibuf = NULL;
+    }
+    return rc;
+}
+
+int f_lfa_aafw(F_LFA_ABD_t *abd, int trg_ix, off_t off, int val, int *old) {
+    int rc;
+
+    abd->ops[0].in32 = val;
+    rc = fi_fetch_atomic(
+            abd->lfa->ep,
+            &abd->ops[0].in32, 1, abd->ops_mr_dsc,
+            &abd->ops[0].out32, abd->ops_mr_dsc,
+            abd->tadr[trg_ix], off, abd->srv_key,
+            FI_INT32, FI_SUM, NULL);
+    if (rc) {
+        LOG_FIERR(rc, "fetch_atomic(add)");
+        return rc;
+    }
+    ON_FIERR(rc = _wait_cq(abd->lfa), return rc, "atomiq cq");
+    if (old) 
+        *old = abd->ops[0].out32;
+
+    return 0;
+}
+
+int f_lfa_aafl(F_LFA_ABD_t *abd, int trg_ix, off_t off, long val, long *old) {
+    int rc;
+
+    abd->ops[0].in64 = val;
+    rc = fi_fetch_atomic(
+            abd->lfa->ep,
+            &abd->ops[0].in64, 1, abd->ops_mr_dsc,
+            &abd->ops[0].out64, abd->ops_mr_dsc,
+            abd->tadr[trg_ix], off, abd->srv_key,
+            FI_INT64, FI_SUM, NULL);
+    if (rc) {
+        LOG_FIERR(rc, "fetch_atomic(add)");
+        return rc;
+    }
+    ON_FIERR(rc = _wait_cq(abd->lfa), return rc, "atomiq cq");
+    if (old) 
+        *old = abd->ops[0].out64;
+
+    return 0;
+}
+
+int f_lfa_addw(F_LFA_ABD_t *abd, int trg_ix, off_t off, int val) {
+    int rc;
+
+    abd->ops[0].in32 = val;
+    rc = fi_atomic(
+            abd->lfa->ep,
+            &abd->ops[0].in32, 1, abd->ops_mr_dsc,
+            abd->tadr[trg_ix], off, abd->srv_key,
+            FI_INT32, FI_SUM, NULL);
+    if (rc) {
+        LOG_FIERR(rc, "fetch_atomic(add)");
+        return rc;
+    }
+    ON_FIERR(rc = _wait_cq(abd->lfa), return rc, "atomiq cq");
+
+}
+
+int f_lfa_addl(F_LFA_ABD_t *abd, int trg_ix, off_t off, long val) {
+    int rc;
+
+    abd->ops[0].in64 = val;
+    rc = fi_atomic(
+            abd->lfa->ep,
+            &abd->ops[0].in64, 1, abd->ops_mr_dsc,
+            abd->tadr[trg_ix], off, abd->srv_key,
+            FI_INT64, FI_SUM, NULL);
+    if (rc) {
+        LOG_FIERR(rc, "fetch_atomic(add)");
+        return rc;
+    }
+    ON_FIERR(rc = _wait_cq(abd->lfa), return rc, "atomiq cq");
+
+    return 0;
+}
+
+int f_lfa_casw(F_LFA_ABD_t *abd, int trg_ix, off_t off, int val, int exp, int *rval) {
+}
+
+int f_lfa_casl(F_LFA_ABD_t *abd, int trg_ix, off_t off, long val, long exp, long *rval) {
+}
+
+/* 
+   Atomic bit_find_clear_and_set: find first clear bit, starting from offset, and set it
+   Note of for the efficiency sake, there's no 64-bit (long) variant of this function.
+   We assume that wotking on words (32-bit) gives us less contention for a given place in memory
+    abd:    blob descriptor
+    trg_ix: index of target node
+    off:    offset of the 1st word of the bit field
+    boff:   intitial bit offset (hopefully it will be clear!)
+    bsize:  max number of bits to scan
+   Return:
+    >= 0:     - offset of the found clear bit
+    -ENOSPACE - no free bits found
+    <0:       - uh-oh.... 
+*/
+int f_lfa_bfcs(F_LFA_ABD_t *abd, int trg_ix, off_t off, int boff, int bsize) {
+    int rc, wrap = 0;
+    uint32_t bit = boff%32;
+    off_t bw = boff/32;
+    uint32_t bmask, cmask, bn = boff, bmax = bsize - 1;
+
+    while (bn < bmax) {
+        abd->ops[0].in32 = bmask = 1<<bit;
+        cmask = ~bmask;
+
+        rc = fi_fetch_atomic(
+                abd->lfa->ep,
+                &abd->ops[0].in32, 1, abd->ops_mr_dsc,
+                &abd->ops[0].out32, abd->ops_mr_dsc,
+                abd->tadr[trg_ix], off + bw, abd->srv_key,
+                FI_INT32, FI_BOR, NULL);
+        if (rc) {
+            LOG_FIERR(rc, "fetch_atomic(bor)");
+            return rc;
+        }
+        ON_FIERR(rc = _wait_cq(abd->lfa), return rc, "atomiq cq");
+
+        if (abd->ops[0].out32 & bmask) {
+            // oops, somebody had already set this bit
+            int clear = __builtin_ffs(~abd->ops[0].out32);
+            if (!clear) {
+                // np clear bits in this word, go to next
+                bit = 0;
+                bn = ++bw*32;
+            } else {
+                // found clear bit somewhere in current word
+                bit = clear - 1;
+                bn = bw*32 + bit;
+            } 
+        } else {
+            // success, return bit number
+            return bw*32 + bit;
+        }
+        if (bn > bmax) {
+            if (!wrap) {
+                wrap++;
+                bn = bit = bw = 0;
+                bmax = boff;
+            } else {
+                break;
+            }
+        }
+    }
+
+    // oopsie, didn't fine any clear bit even afet full scan
+    return -ENOSPC;
+}
+
+/*
+   Detach from remote atomic blob
+    If free != 0, free all memory buffers 
+*/
+int f_lfa_detach(F_LFA_ABD_t *abd, int free) {
+    return 0;
+}
+
+/*
+   Deregister local buffers and free all lf resources
+    If free != 0, free all memory buffers 
+*/
+int f_lfa_deregister(F_LFA_ABD_t *abd, int free) {
+    return 0;
+}
+
+/*
+   Close evrything lf-related, free memory 
+*/
+int f_lfa_destroy(F_LFA_DESC_t *lfa) {
+    return 0;
+}
+
+
+
