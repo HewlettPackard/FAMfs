2c072fc8eee1c75d90cd662ed7086491500b83b1
Oleg Neverovitch <oleg.neverovitch@hpe.com>
2020-07-13 11:32:01 -0600

    added encode rq and queues structs etc

--- server/src/f_encode_recovery.h
+++ server/src/f_encode_recovery.h
@@ -7,10 +7,59 @@
 #ifndef F_ENCODE_RECOVERY_H
 #define F_ENCODE_RECOVERY_H
 
+#include <stdint.h>
+#include <pthread.h>
+#include "list.h"
+
 struct ec_worker_data {
 	F_LO_PART_t 		*lp;	/* layout partition pointer */
 	struct f_stripe_set	ss;	/* stripe set, ss->stripes released by the worker thread */
 };
+
+
+typedef enum {
+    F_EDR_EMPTY,    // Use for preallocated RQs
+    F_EDR_READ,     // lf_read
+    F_EDR_CALC,     // parity calcualtors worker pool
+    F_EDR_WRITE,    // lf_write
+    F_EDR_DONE      // completed
+} F_EDR_STATE_t;
+
+struct f_edr_;
+typedef int (*f_edr_cb_)(struct f_edr_ *rq);
+
+//
+// Encode-Decode Request
+// While request is being processed, it does not belong to any Q
+// lf_read/write will provide *Rq in cq context field
+typedef struct f_edr_ {
+    struct list_head        list;       // Request list links
+    struct ec_worker_data   wdata;      // Worker data: stripe set and lyaout
+    F_EDR_STATE_t           state;      // Request state
+    f_edr_cb_               completion; // Operation end callback
+    ssize_t                 bsize;      // Buffer size
+    char                    **bvec;     // Buffer vector of this request
+    int                     nvec;       // Number of buffers in the vector
+    int                     busy;       // Number of buffers in transition (under I/O or calculations)
+                                        //  when this drops to 0, completion cb is called that will 
+                                        //  presumably chnage request's state (and queue asignment)
+    int                     status;     // General completion status
+    int                     nerr;       // Number of errors (only if status != 0)
+
+} F_EDR_t;
+
+//
+// EDR operations queue
+// Need two: submitted requests and completed ones (an optional Q for pre-allocated RQs)
+// New(Rq) or Get(Rq from PreQ) -> SubmitQ -> thread removes it from Sq and starts operation -> 
+//   Rq completes -> CB -> CompletedQ -> another thread removes it from Cq -> CB -> etc.
+typedef struct f_edr_opq_ {
+    pthread_mutex_t     lock;
+    pthread_cond_t      cond;
+    struct list_head    queue;
+    int                 size;
+} F_EDR_OPQ_t;
+
 
 /*
  * Recover a set of stripes worker function
