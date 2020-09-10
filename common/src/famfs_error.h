443413a1f33d11c2cbf337d51043ceda8f69d7ad
Dmitry Ivanov <d.ivanov@hpe.com>
2020-09-10 17:34:59 -0600

    famfs_error.h: restore changes rolled back by commit ef31a154

--- common/src/famfs_error.h
+++ common/src/famfs_error.h
@@ -21,72 +21,58 @@
     } while (0)
 #endif
 
-#define DEBUG_LVL_(verbosity, lvl, fmt, ...) \
-do { \
-    if ((verbosity) >= (lvl)) \
-        printf("famfs: %s:%d: %s: " fmt "\n", \
-               __FILE__, __LINE__, __func__, ## __VA_ARGS__); \
+#define DEBUG_LVL_(verbosity, lvl, fmt, ...)     \
+do {                                             \
+    if ((verbosity) >= (lvl))                    \
+        DEBUG(fmt, ## __VA_ARGS__);              \
 } while (0)
 
 #ifndef ERROR
-#define ERROR(fmt, ...) \
-    do {								\
-	fprintf(stderr, "famfs error: %s:%d: %s: " fmt "\n",	\
-		__FILE__, __LINE__, __func__, ## __VA_ARGS__);	\
+#define ERROR(fmt, ...)                          \
+    do {                                         \
+        fprintf(stderr, "famfs error: %s:%d: %s: " fmt "\n",   \
+		__FILE__, __LINE__, __func__, ## __VA_ARGS__); \
     } while (0)
 #endif
 
-#define ON_ERROR(action, msg, ...)          \
-    do {                                    \
-        int __err;                          \
-        if ((__err = (action))) {           \
-            fprintf(stderr, #msg ": %d - %m\n", ## __VA_ARGS__, __err); \
-            exit(1);                        \
-        }                                   \
+#define _op_exit(arg)   exit(arg)
+#define _op_return(arg) return(arg)
+#define _op_ret(arg)    return
+#define _op_retabs(arg) return(-abs(arg))
+
+#define ON_ERROR_(action, op, arg, msg, ...)     \
+    do {                                         \
+        int __err;                               \
+        if ((__err = (action))) {                \
+            ERROR(#msg ": %d - %m",              \
+                  ##__VA_ARGS__, __err);         \
+            op(arg);                             \
+        }                                        \
     } while (0);
 
-#define ON_ERROR_RET(action, msg, ...)      \
-    do {                                    \
-        int __err;                          \
-        if ((__err = (action))) {           \
-            fprintf(stderr, #msg ": %d - %m\n", ## __VA_ARGS__, __err); \
-            return;                         \
-        }                                   \
-    } while (0);
+#define ON_ERROR(action, msg, ...)               \
+    ON_ERROR_(action, _op_exit, 1, msg, ## __VA_ARGS__)
 
-#define ON_ERROR_RV(action, ret_val, msg, ...) \
-    do {                                       \
-        int __err;                             \
-        if ((__err = (action))) {              \
-            fprintf(stderr, #msg ": %d - %m\n", ## __VA_ARGS__, __err); \
-            return (ret_val);                  \
-        }                                      \
-    } while (0);
+#define ON_ERROR_RET(action, msg, ...)           \
+    ON_ERROR_(action, _op_ret, , msg, ## __VA_ARGS__)
 
-#define ON_ERROR_RC(action, msg, ...)          \
-    do {                                       \
-        int __err;                             \
-        if ((__err = (action))) {              \
-            fprintf(stderr, #msg ": %d - %m\n", ## __VA_ARGS__, __err); \
-            return -abs(__err);                    \
-        }                                      \
-    } while (0);
+#define ON_ERROR_RV(action, ret_val, msg, ...)   \
+    ON_ERROR_(action, _op_return, ret_val, msg, ## __VA_ARGS__)
 
-#define ON_NOMEM_RET(action, msg, ...) \
-    do {                                       \
-        if ((NULL == (action))) {              \
-            fprintf(stderr, #msg ": NULL - %m\n", ## __VA_ARGS__); \
-            return (-ENOMEM);                  \
-        }                                      \
-    } while (0);
+#define ON_ERROR_RC(action, msg, ...)            \
+    ON_ERROR_(action, _op_retabs,  (int)(action), msg, ## __VA_ARGS__)
+
+#define ON_NOMEM_RET(action, msg, ...)           \
+    ON_ERROR_(!(action), _op_return, -ENOMEM, msg, ## __VA_ARGS__)
 
 
-#define    ASSERT(x)                    \
-    do {                                \
-        if (!(x))    {                  \
-            fprintf(stderr, "ASSERT failed %s:%s(%d) " #x "\n", __FILE__, __FUNCTION__, __LINE__); \
-            exit(1);                    \
-        }                               \
+#define    ASSERT(x)                             \
+    do {                                         \
+        if (!(x))    {                           \
+            fprintf(stderr, "ASSERT failed %s:%s(%d) " #x "\n", \
+              __FILE__, __FUNCTION__, __LINE__); \
+            exit(1);                             \
+        }                                        \
     } while (0);
 
 #define err(str, ...) fprintf(stderr, str "\n", ## __VA_ARGS__)
