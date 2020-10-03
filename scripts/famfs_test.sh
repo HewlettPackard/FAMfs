c29856488c347c6c4cbed6b2ed2eebc2ff0ff069
Dmitry Ivanov <dmitry.ivanov2@hpe.com>
2020-10-02 20:50:04 -0600

    famfs_test: fix -t option for MPICH

--- scripts/famfs_test.sh
+++ scripts/famfs_test.sh
@@ -304,7 +304,7 @@ if ((ompi)); then
 fi
 if ((mpich)); then
   if ((oTCP)); then
-    oMPIchEnv+=" MPIR_CVAR_OFI_USE_PROVIDER=sockets"
+    oMPIchEnv+=" -x MPIR_CVAR_OFI_USE_PROVIDER=sockets"
   fi
 fi
 if ((tVERBOSE)); then
