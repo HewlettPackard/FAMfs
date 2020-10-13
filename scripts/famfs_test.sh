HewlettPackard/FAMfs

commit ba09fdfea2378732d42e62d30dff9f50686f725b

famfs_test: add parser for two_passes (-Q) option; fix do write once

scripts/famfs_test.sh
@@ -82,6 +82,50 @@ function update_ini() {
   sed -r -i "/^\[$s\]\$/,/^\[/{s/^$k = .* (; .*)\$/$k = $v \1/; t; s/^$k = [^\s]*\$/$k = $v/}" ${FAMFS_CONF}
 }
 
+# parseTwoPasses $optionQ aCNdelta aRECONF aSEQ
+function parseTwoPasses() {
+  local p token pref val
+  let p=0
+  while IFS=: read -a token -d,
+  do
+    case ${#token[@]} in
+      1) val=${token[0]}; pref= ;;
+      2) pref=${token[0]}; val=${token[1]}; ;;
+      *) echo "Syntax error in two_passes option, token >${token[@]}<"; exit 1 ;;
+    esac
+    # prefixes: CN-=<number> or CONF=<conf_file>
+    if [ -z "$pref" ]; then
+      eval $"$2"[p]=0
+      eval $"$3"[p]=""
+    else
+      prop=${pref#*=}
+      [ -z "$prop" ] && { echo "Syntax error in two_passes option @$p, missing '=*' in prefix >${pref}<"; exit 1; }
+      # CN down?
+      if [[ "$pref" =~ ^"CN-="[0-9]+$ ]]; then
+        eval $"$2"[p]=$prop
+        eval $"$3"[p]=""
+      elif [[ "$pref" =~ ^CONF ]]; then
+        eval $"$2"[p]=0
+        eval $"$3"[p]=$prop
+      else
+        echo "Syntax error in two_passes option @$p, wrong prefix >${pref}<"
+        exit 1
+      fi
+    fi
+    # values: SEQ or RND
+    case "$val" in
+      [sS] | SEQ) eval $"$4"[p]=1 ;;
+      [rR] | RND) eval $"$4"[p]=0 ;;
+      *) echo "Syntax error in two_passes option @$p, bad value >${val}<"; exit 1 ;;
+    esac
+    if ((tVERBOSE)); then
+      eval echo "two_passes: pass $((p+1)) - "'CN-=${'${2}'[p]} CONF=${'${3}'[p]} SEQ=${'${4}'[p]}'
+    fi
+    ((p++))
+  done <<< "$1,"
+}
+
+
 #
 # Working DIR
 #
@@ -236,17 +280,27 @@ for w in $oWRITES; do TXSZ[$i]=$(getval $w); if ((TXSZ[i] > max_tx)); then ((max
 ((i = 0))
 for r in $oREADS; do RDSZ[$i]=$(getval $r); ((i++)); done
 
+srv_opt="$oExtraSrvOpt"
+((tVERBOSE=0))
+if ((oVERBOSE)); then
+    export tVERBOSE=1
+    srv_opt="$srv_opt -v 6"
+fi
+
 nPatterns=1
 if [ ! -z "$oTwoPasses" ]; then
-  oSEQ=1 # SEQ first
   nPatterns=2
+  aCNdelta=()
+  aRECONF=()
+  aSEQ=()
+  # parse option into arrays: CNdelta, RECONF filename, SEQ/RND bit
+  parseTwoPasses "$oTwoPasses" aCNdelta aRECONF aSEQ
 fi
 
 blksz=$(getval $oBLOCK)
 wup=$(getval $oWARMUP)
 seg=$(getval $oSEGMENT)
 extsz=$(getval $ExtSize)
-((tVERBOSE=0))
 ((tIOR=0))
 
 # mpich or ompi?
@@ -264,11 +318,7 @@ if ((!${#SrvIter[*]})); then SrvIter[0]=$ns; fi
 if ((!${#ClnIter[*]})); then ClnIter[0]=$nc; fi
 
 unset IFS
-srv_opt="$oExtraSrvOpt"
-if ((oVERBOSE)); then
-    export tVERBOSE=1
-    srv_opt="$srv_opt -v 6"
-fi
+
 ((err=0))
 TEST_BIN="${TEST_DIR}/libexec/${oAPP}"
 if [ ! -x ${TEST_BIN} ]; then
@@ -294,7 +344,6 @@ ITR=""
 if [[ "$oAPP" =~ ior ]]; then
   ((tIOR=1))
   cycles=1
-  ITR="-i $oCycles"
 else
   ((fstype<1)) && { echo "Wrong fs type:$oFStype"; exit 1; }
   ((cycles = oCycles))
@@ -302,7 +351,7 @@ fi
 clMPImap=
 if ((ompi)); then
   oMPIchEnv="${oMPIchEnv} --bind-to none -x FI_MR_CACHE_MONITOR -x LD_LIBRARY_PATH -x PATH -x MPIROOT"
-  oMPIchEnv+=" -x TEST_BIN -x SRV_OPT -x ZHPEQ_HOSTS"
+  oMPIchEnv+=" -x TEST_BIN -x SRV_OPT -x ZHPEQ_HOSTS -x UNIFYCR_CONFIGFILE"
   if ((oTCP)); then
     oMPIchEnv+=" --mca btl ^ofi,openib,vader --mca mtl ^ofi,psm,psm2,portals4 --mca pml ^ucx --mca btl_ofi_disable_sep true --mca mtl_ofi_enable_sep 0"
   else
@@ -346,9 +395,9 @@ if [ ! -f "$FAMFS_CONF" ]; then
     update_ini devices single_ep $SingleEP
     echo "Layout moniker: $moniker"
 fi
-FP_FAMFS_CONF="${PWD}/${FAMFS_CONF}"
-export UNIFYCR_CONFIGFILE=$FP_FAMFS_CONF
-echo "configuration file: $FP_FAMFS_CONF"
+FP_FAMFS_CONF_DEF="${PWD}/${FAMFS_CONF}"
+export UNIFYCR_CONFIGFILE=$FP_FAMFS_CONF_DEF
+echo "configuration file: $UNIFYCR_CONFIGFILE"
 
 for ((si = 0; si < ${#SrvIter[*]}; si++)); do
     Servers=`make_list "$hh" "${SrvIter[$si]}" "$oNodeSuffix"`
@@ -381,11 +430,7 @@ for ((si = 0; si < ${#SrvIter[*]}; si++)); do
         nc=`count $Clients`
 
         for ((i = 0; i < ${#RANK[*]}; i++)); do
-            export Ranks="${RANK[$i]}"
-            if ((ompi)); then
-                ((tnc=nc*Ranks))
-                cMPImap="${clMPImap} -n $tnc"
-            fi
+            nRanks="${RANK[$i]}"
             for ((j = 0; j < ${#TXSZ[*]}; j++)); do
                 transfersz=${TXSZ[$j]}
                 dsc="[$nc*${RANK[$i]}]->$ns Block=$blksz Segments=$seg"
@@ -413,16 +458,37 @@ for ((si = 0; si < ${#SrvIter[*]}; si++)); do
                 if ((wup == 0)); then
                     dsc="$dsc <no warmup>"
                 else
-                    ((tIOR)) && ITR="-i $((oCycles+1))" || wu="-W $wup"
-                    ((tIOR)) || dsc="$dsc WARMUP=$wup"
+                    if ((tIOR)); then
+                        echo "Error: ior has no support for warm-up!"
+                        exit 1
+                    else
+                        wu="-W $wup"
+                        dsc="$dsc WARMUP=$wup"
+                    fi
                 fi
 
                 # run Client app one or two times, with specific i/o pattern
                 for ((iPattern=0; iPattern<nPatterns; iPattern++)); do
 
+                    ((tnc=nc*nRanks))
                     if ((nPatterns>1)); then
-                        # TODO: Parse oTwoPasses (-Q) into SEQ array
-                        ((iPattern==0))&& oSEQ=1 || oSEQ=0
+                        vSEQ=${aSEQ[iPattern]}
+                        ((tnc-=${aCNdelta[iPattern]}))
+                        FP_FAMFS_CONF=${aRECONF[iPattern]}
+                        [ -z "$FP_FAMFS_CONF" ]&& FP_FAMFS_CONF=$FP_FAMFS_CONF_DEF
+                        # reconfigure server?
+                        if [[ "$FP_FAMFS_CONF" != "$UNIFYCR_CONFIGFILE" ]]; then
+                            dsc="$dsc RECONF"
+                            export UNIFYCR_CONFIGFILE=$FP_FAMFS_CONF
+                            iPattern=-1 ${SCRIPT_DIR}/test_cycle.sh || { echo "Failed to reconfigure Server!"; exit 1; }
+                        fi
+                    else
+                        vSEQ=$oSEQ
+                    fi
+                    if ((ompi)); then
+                        cMPImap="${clMPImap} -n $tnc"
+                    else
+                        cMPImap="-np $tnc"
                     fi
 
                     if [ -z "${RDSZ[$j]}" ]; then
@@ -434,9 +500,11 @@ for ((si = 0; si < ${#SrvIter[*]}; si++)); do
                             reads="${RDSZ[$j]}"
                         fi
                     fi
+                    vCycles=$cycles
                     if ((nPatterns==1)); then
                         ((tIOR))&& reads="-w -r" || reads="-w ${TXSZ[$j]} -r ${RDSZ[$j]}"
                     elif ((iPattern==0)); then
+                        vCycles=1
                         transfersz=${TXSZ[$j]}
                         ((tIOR))&& reads="-w" || reads="-w $transfersz"
                     else
@@ -453,7 +521,7 @@ for ((si = 0; si < ${#SrvIter[*]}; si++)); do
                             vfy="-V"
                         fi
                     fi
-                    if (($oSEQ)); then
+                    if (($vSEQ)); then
                         ((tIOR)) && seq="" || seq="-S 1"
                         dsc="$dsc SEQ"
                     else
@@ -462,7 +530,11 @@ for ((si = 0; si < ${#SrvIter[*]}; si++)); do
                         dsc="$dsc RANDOM"
                     fi
 
-                    for ((k = 0; k < cycles; k++)); do
+                    if ((tIOR)); then
+                        ITR="-i $vCycles"
+                    fi
+
+                    for ((k = 0; k < vCycles; k++)); do
                         ((mem = (nc*RANK[i]*seg*blksz + nc*RANK[i]*wup)/ns))
                         ((mem = (mem/1024/1024/1024 + 1)*1024*1024*1024))
                         # TODO: check device size >= $mem
@@ -485,6 +557,7 @@ for ((si = 0; si < ${#SrvIter[*]}; si++)); do
                         export SRV_OPT="$srv_opt"
                         export iPattern
                         export nPatterns
+                        export tStopServer=$((iPattern==nPatterns-1 && k==vCycles-1))
 
                         ((tIOR)) \
                           && opts="-o ${tstFileName} $BLK $SEG $WSZ $RSZ $VFY $PTR $SEQ $ITR -O unifycr=$fstype -a POSIX -g $oExtraOpt" \

scripts/test_cycle.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 TESTCYCLE_PID_FN="/tmp/testcycle.pid"
-if ((iPattern==0)); then
-  $mpirun $mpi_hosts ${all_n} $mpi_ppn 1 $oMPIchEnv /bin/bash -c "${SCRIPT_DIR}/cleanup.sh"
+sts=1
 
+function start_server() {
   ((tVERBOSE)) && echo "$mpirun $mpi_hosts $AllNodes $mpi_ppn 1 $oMPIchEnv /bin/bash -c \"ulimit -s 1024; $SRV_BIN ${SRV_OPT}\" 2>>$MPI_LOG 1>>$SRV_LOG"
   echo "Starting unifycrd..."
   $mpirun $mpi_hosts $AllNodes $mpi_ppn 1 $oMPIchEnv /bin/bash -c "ulimit -s 1024; $SRV_BIN ${SRV_OPT}" 2>>$MPI_LOG 1>>$SRV_LOG &
@@ -33,35 +33,61 @@ if ((iPattern==0)); then
   echo
   echo "### $DSC" >>$MPI_LOG
   echo "### $DSC" >>$SRV_LOG
-fi
+}
 
-echo "### $DSC" >>$TEST_LOG
-TEST_BASH_ARG="$cNUMAshell $TEST_BIN $TEST_OPTS"
-((tVERBOSE)) && echo "$mpirun $cMPImap $mpi_hosts $Clients $mpi_ppn $Ranks $oMPIchEnv /bin/bash -c \"$TEST_BASH_ARG\" 2>>$MPI_LOG 1>>$TEST_LOG"
-echo "Starting test..."
-$mpirun $cMPImap $mpi_hosts $Clients $mpi_ppn $Ranks $oMPIchEnv /bin/bash -c "$TEST_BASH_ARG" 2>>$MPI_LOG 1>>$TEST_LOG
-
-if (($? == 0))
-then
-    echo "### OK" >>$TEST_LOG
-    echo "Test completed successfully"
-    sts=0
-else
-    echo "### ERRORS" >>$TEST_LOG
-    echo "Test failed"
-    # sleep 10000
-    sts=1
-fi
-
-if ((iPattern==nPatterns-1)); then
+function stop_server() {
   pid=$(cat $TESTCYCLE_PID_FN)
   echo "Stopping servers ($pid)"
   kill -INT $pid
   while ps -p $pid 2>/dev/null 1>/dev/null; do
     echo "Waiting for unifycrd to exit"
-    sleep 10
+    sleep 6
     kill -INT $pid 2>/dev/null 1>/dev/null
   done
+}
+
+function run_test() {
+  echo "### $DSC" >>$TEST_LOG
+  TEST_BASH_ARG="$cNUMAshell $TEST_BIN $TEST_OPTS"
+  ((tVERBOSE)) && echo "$mpirun $cMPImap $mpi_hosts $Clients $oMPIchEnv /bin/bash -c \"$TEST_BASH_ARG\" 2>>$MPI_LOG 1>>$TEST_LOG"
+  echo "Starting test..."
+  $mpirun $cMPImap $mpi_hosts $Clients $oMPIchEnv /bin/bash -c "$TEST_BASH_ARG" 2>>$MPI_LOG 1>>$TEST_LOG
+
+  if (($? == 0))
+  then
+    echo "### OK" >>$TEST_LOG
+    echo "Test completed successfully"
+    sts=0
+  else
+    echo "### ERRORS" >>$TEST_LOG
+    echo "Test failed"
+    # sleep 10000
+    sts=1
+  fi
+}
+
+# restart Server
+if ((iPattern==-1)); then
+  stop_server
+  start_server
+  sts=0
+  echo "    Server restarted" >>$TEST_LOG
+fi
+
+# start Server
+if ((iPattern==0)); then
+  $mpirun $mpi_hosts ${all_n} $mpi_ppn 1 $oMPIchEnv /bin/bash -c "${SCRIPT_DIR}/cleanup.sh"
+  start_server
+fi
+
+# run test
+if ((iPattern>=0)); then
+  run_test
+fi
+
+# stop Server
+if ((tStopServer)); then
+  stop_server
 fi
 
 exit $sts

commit 9322a20aa17ec0c7e780b7db130cb656665326b8

famfs_test: fix test_cycle.sh broken by commit ba09fdf

scripts/famfs_test.sh
@@ -369,6 +369,7 @@ esac
 ITR=""
 if [[ "$oAPP" =~ ior ]]; then
   ((tIOR=1))
+  ((oVFY))&& { echo "Can't combine verify & random"; exit 1; }
   cycles=1
 else
   ((fstype<1)) && { echo "Wrong fs type:$oFStype"; exit 1; }
@@ -470,6 +471,7 @@ for ((si = 0; si < ${#SrvIter[*]}; si++)); do
                         dsc="$dsc Reads=${RDSZ[$j]}"
                     fi
                 fi
+                ((tVFY=oVFY))&& dsc="$dsc with VFY"
                 if ((nPatterns>1)); then
                     dsc="$dsc $oTwoPasses"
                 fi
@@ -541,32 +543,31 @@ for ((si = 0; si < ${#SrvIter[*]}; si++)); do
                             reads="${RDSZ[$j]}"
                         fi
                     fi
+                    # test pattern
                     vCycles=$cycles
+                    ioPatternRW=
                     if ((nPatterns==1)); then
-                        ((tIOR))&& reads="-w -r" || reads="-w ${TXSZ[$j]} -r ${RDSZ[$j]}"
+                        ioPatternRW=RW
                     elif ((iPattern==0)); then
-                        vCycles=1
-                        transfersz=${TXSZ[$j]}
-                        ((tIOR))&& reads="-w" || reads="-w $transfersz"
+                        vCycles=$((tVFY?2:1))
+                        ioPatternRW=W
                     else
-                        transfersz=${RDSZ[$j]}
-                        ((tIOR))&& reads="-r" || reads="-r $transfersz"
+                        ioPatternRW=R
                     fi
 
                     vfy=""
-                    if ((oVFY)); then
-                        dsc="$dsc with VFY"
+                    if ((tVFY)); then
                         if ((tIOR)); then
                             ((iPattern==0))&& vfy="-G 1234567890" || vfy="-R -G 1234567890"
                         else
                             vfy="-V"
                         fi
                     fi
+
                     if (($vSEQ)); then
                         ((tIOR)) && seq="" || seq="-S 1"
                         dsc="$dsc SEQ"
                     else
-                        ((oVFY)) && ((tIOR)) && { echo "Can't combine verify & random"; exit 1; }
                         ((tIOR)) && seq="-z" || seq="-S 0"
                         dsc="$dsc RANDOM"
                     fi
@@ -579,6 +580,14 @@ for ((si = 0; si < ${#SrvIter[*]}; si++)); do
                         ((mem = (nc*RANK[i]*seg*blksz + nc*RANK[i]*wup)/ns))
                         ((mem = (mem/1024/1024/1024 + 1)*1024*1024*1024))
                         # TODO: check device size >= $mem
+
+                        # set i/o pattern
+                        case $ioPatternRW in
+                        RW ) ((tIOR))&& reads="-w -r" || reads="-w ${TXSZ[$j]} -r ${RDSZ[$j]}" ;;
+                        W ) transfersz=${TXSZ[$j]}; ((tIOR))&& reads="-w" || reads="-w $transfersz" ;;
+                        R ) transfersz=${RDSZ[$j]}; ((tIOR))&& reads="-r" || reads="-r $transfersz" ;;
+                        *) break ;;
+                        esac
 
                         BLK="-b $blksz"
                         SEG="-s $seg"
@@ -619,6 +628,12 @@ for ((si = 0; si < ${#SrvIter[*]}; si++)); do
                         else
                             ((err++))
                             echo "Finished with ERRORS"
+                        fi
+
+                        # hack: amend ioPatternRW for read w/verify pass just after write
+                        if ((tVFY && nPatterns>1 && iPattern==0))&& [[ ioPatternRW == W ]]; then
+                            ioPatternRW=R
+                            echo "=== verify data after write ===" >> $TEST_LOG
                         fi
                     done
                 done

scripts/test_cycle.sh
@@ -23,7 +23,7 @@ function start_server() {
       ((waiting > 0))&& sleep $_dt
       ((waiting += _dt))
       ssh -q "${hst}" exit || { echo "Cannot ssh to ${hst}"; exit 1; }
-      let _nf=$(ssh -q r1c1t5n1 find /tmp -maxdepth 1 -type f -name 'unifycrd.running.*' 2>/dev/null | wc -l)
+      let _nf=$(ssh -q "${hst}" find /tmp -maxdepth 1 -type f -name 'unifycrd.running.*' 2>/dev/null | wc -l)
       ((_nf>1))&& { echo "Please clean ${hst}:/tmp/"; exit 1; }
       ((_nf==0))
     do